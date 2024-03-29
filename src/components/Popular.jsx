0a70bcf92bc2bc5091dfc1999a10a41acd0769b7
Miguel Leonar <migsldev@gmail.com>
2024-03-29 14:53:26 +1100

    updated and modified code

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import { useEffect, useState } from "react";
 
 function Popular() {
-  return (
-    <div>
-      
-    </div>
-  )
+
+    const [popular, setPopular] = useState([]);
+
+    useEffect(() => {
+        getPopular(); //running getPopular once the components get mounted
+    }, []); // adding empty array to indicate only run the components when it gets mounted
+
+    const getPopular = async () => {
+        const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+        );
+        const data = await api.json();
+        //console.log(data);
+        setPopular(data.recipes)
+    };
+
+  return <div>
+
+    {popular.map((recipe) => {
+        return (
+            <div key={recipe.id}>
+                <p>{recipe.title}</p>
+            </div>
+        );
+    })}
+    </div>;
 }
 
-export default Popular
+export default Popular;
