579b194b9f894d02ff336af3c1337b10f468df2a
Miguel Leonar <migsldev@gmail.com>
2024-03-29 18:08:33 +1100

    updated JS code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Pages from "./pages/Pages";
+import Category from "./components/Category";
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Category />
       <Pages />
     </div>
   );
